8e6ac8d69235c08b536a31c8f1a07150db22f3a2
johnlo23 <john@jlny.org>
2022-11-11 20:58:01 -0500

    All functions now working. Comments in place.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from datetime import datetime
-import string
 import pandas as pd
 
 INPUT_FILE = "addresses.txt"
-OUTPUT_FILE = "address_list.csv"
 
 
 # Creates objects to hold a mailing address
@@ -18,7 +16,7 @@ class Address(str):
         self.state = ""
         self.postcode = ""
 
-    # Return all address parts joined with space seperator
+    # Return address parts joined with space seperator
     def normal_address(self):
         return " ".join((self.number, self.prefix, self.street, self.suffix,
                          self.city, self.state, self.postcode)).replace("  ", " ").replace("  ", " ")
@@ -32,8 +30,9 @@ class Address(str):
 
 # Determine if a string is a standard directional abbreviation
 def is_directional(f_string):
+    directionals = ('N', 'S', 'E', 'W', 'NE', 'NW', 'SE', 'SE')
     # Returns True if string is in directional tuple
-    return f_string.upper().strip() in ('N', 'S', 'E', 'W', 'NE', 'NW', 'SE', 'SE')
+    return f_string.upper().strip() in directionals
 
 
 # Determine if a string matches standard Zip Code format
@@ -52,18 +51,43 @@ def is_postalcode(f_string):
 
 
 # Get a unique output filename based on the date and time
-def outfile_name():
+def datetime_name():
     dt = datetime.now()
     date = ".".join((str(dt.year), str(dt.month), str(dt.day)))
     time = ".".join((str(dt.hour), str(dt.minute), str(dt.second)))
     return f"address_{date}_{time}.csv"
 
 
+def get_filename(f_prompt, f_default):
+    print(f"{f_prompt} (blank for {f_default}): ", end='')
+    f_in = input()
+    if f_in == '':
+        return f_default
+    else:
+        return f_in
+
+
+# Main body of program
 def main():
+    # Display program introduction
+    print("Address Conversion")
+    print("- "*10)
+    print("This program accepts a file with a list of normalized addresses.")
+    print("It will output a CSV file of addresses split into component parts.")
+    print()
+
+    infile_name = get_filename("Enter the file name to import", INPUT_FILE)
+
     # Open the address file for reading
-    with open(INPUT_FILE, "r", encoding='UTF-8') as in_file:
-        # Build address list from file
-        address_lines = [readline.rstrip() for readline in in_file]
+    try:
+        with open(infile_name, "r", encoding='UTF-8') as in_file:
+            # Build address list from file
+            address_lines = [readline.rstrip() for readline in in_file]
+
+    except FileNotFoundError:
+        print(f"The file {infile_name} was not found.")
+        input("Press <enter> to quit")
+        quit()
 
     # Initiate empty address list
     address_list = []
@@ -111,11 +135,31 @@ def main():
             else:
                 add.street = add_split.pop() + " " + add.street
 
+        # Append each address as dictionary to the list
         address_list.append(add.address_parts())
 
+    # Build a pandas data frame from the address list of dictionaries
     df = pd.DataFrame(address_list)
-    df.to_csv(outfile_name())
 
+    # Remove duplicate rows from the data frame and count the difference
+    file_len = df.shape[0]
+    df.drop_duplicates(inplace=True)
+    dup_len = file_len - df.shape[0]
 
+    # Get output filename
+    outfile_name = get_filename("Enter the file name to export", datetime_name())
+
+    # Output the data frame to a csv file - Assign a heading to the index column
+    df.to_csv(outfile_name, index_label='line_no')
+
+    print()
+    print("Conversion complete")
+    print("- "*9)
+    print(f"{df.shape[0]} Addresses have been converted.")
+    print(f"{dup_len} duplicates have been removed.")
+    print()
+    input("Press <enter> to quit")
+
+# Call main function
 if __name__ == "__main__":
     main()
